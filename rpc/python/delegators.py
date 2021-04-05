1ba555966b40c27f79fb946e13607a53460f6fe3
Dimitrios Siganos <dimitris@siganos.org>
2021-04-05 01:40:11 +0100

    Add script for delegators and delegators_count RPC commands

---
+++ rpc/python/delegators.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--count', action='store_true', default=False,
+                        help='Count delegators only')
+    parser.add_argument("account")
+    return parser.parse_args()
+
+def post(session, params, timeout=5):
+    resp = session.post('http://[::1]:7076', json=params, timeout=5)
+    return resp.json()
+
+args = parse_args()
+
+action = 'delegators'
+if args.count:
+    action = 'delegators_count'
+
+params = {
+  "action": action,
+  "account": args.account
+}
+
+session = requests.Session()
+result = post(session, params)
+print(json.dumps(result, indent=4))
