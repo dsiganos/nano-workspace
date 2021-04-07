7992ea5e0faa2becf66ee346f9f00c0eac749f94
Dimitrios Siganos <dimitris@siganos.org>
2021-04-07 02:58:28 +0100

    Option to ask for peer detail in confirmation_quorum rpc command script

--- rpc/python/confirmation_quorum.py
+++ rpc/python/confirmation_quorum.py
@@ -4,6 +4,14 @@ import requests
 import json
 import argparse
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--peerdetails', action='store_true', default=False,
+                        help='Request peer details')
+    return parser.parse_args()
+
+args = parse_args()
+
 def post(session, params, timeout=5):
     resp = session.post('http://[::1]:7076', json=params, timeout=5)
     return resp.json()
@@ -11,6 +19,9 @@ def post(session, params, timeout=5):
 params = {
   "action": "confirmation_quorum"
 }
+
+if args.peerdetails:
+    params['peer_details'] = 'true'
 
 session = requests.Session()
 result = post(session, params)
