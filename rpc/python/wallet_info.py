cdfb5573d12e8e57ab0986468d37e7c036797dfa
Dimitrios Siganos <dimitris@siganos.org>
2021-04-28 14:29:35 +0100

    wallet_info, pretty printing of funds

--- rpc/python/wallet_info.py
+++ rpc/python/wallet_info.py
@@ -23,3 +23,8 @@ params = {
 session = requests.Session()
 result = post(session, params)
 print(json.dumps(result, indent=4))
+
+balance_in_nano = int(result['balance']) / 1e30
+pending_in_nano = int(result['pending']) / 1e30
+print('Balance in NANO: %s' % balance_in_nano)
+print('Pending in NANO: %s' % pending_in_nano)
