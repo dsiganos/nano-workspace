6bcf43cf824a975894f861e5d947c4a4ad1c1e2e
Dimitrios Siganos <dimitris@siganos.org>
2021-05-22 17:59:51 +0100

    check_peering_nodes.py: spawn connections in background

--- scripts/check_peering_nodes.py
+++ scripts/check_peering_nodes.py
@@ -2,6 +2,9 @@
 import socket
 import sys
 import dns.resolver
+import threading
+
+tmo = 3
 
 def get_all_dns_addresses(url):
     result = dns.resolver.resolve(url, 'A')
@@ -9,18 +12,27 @@ def get_all_dns_addresses(url):
 
 def connect(to):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(3)
-    print('%15s:%-5s ' % to, end='')
+    sock.settimeout(tmo)
     try:
         sock.connect(to)
-        print('connected')
+        print('%15s:%-5s connected' % to)
     except:
-        print('NOT connected')
+        print('%15s:%-5s NOT RESPONDED (within %s secs)' % (*to,  tmo))
+
+def parallel_connect(endpoints):
+    threads = []
+    for ip, port in endpoints:
+        t = threading.Thread(target = lambda : connect((ip, port)))
+        threads.append(t)
+        t.start()
+    return threads
 
 print('Live nodes')
-for node in get_all_dns_addresses('peering.nano.org'):
-    connect((node, 7075))
+live_endpoints = [ (x, 7075) for x in get_all_dns_addresses('peering.nano.org') ]
+for t in parallel_connect(live_endpoints):
+    t.join()
 
 print('Beta nodes')
-for node in get_all_dns_addresses('peering-beta.nano.org'):
-    connect((node, 54000))
+beta_endpoints = [ (x, 54000) for x in get_all_dns_addresses('peering-beta.nano.org') ]
+for t in parallel_connect(beta_endpoints):
+    t.join()
