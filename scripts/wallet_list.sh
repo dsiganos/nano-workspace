96d942b80885c672caf4e2650c0cd4032d66e44d
Dimitrios Siganos <dimitris@siganos.org>
2021-10-16 00:28:26 +0200

    wallet_list.sh: support live, beta and test networks

--- scripts/wallet_list.sh
+++ scripts/wallet_list.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+if [ "$1" != "" -a "$1" != "beta" -a "$1" != "test" ]; then
+    echo beta or test argument expected
+    exit 1
+fi
+
+network=${1:-live}
+data_prefix=${1}
+
+
 dir="`dirname "$0"`"
-"$dir/../build/nano_node" --wallet_list --data_path "$dir/../data"
+"$dir/../build/nano_node" --wallet_list --data_path "$dir/../${data_prefix}data" --network $network
