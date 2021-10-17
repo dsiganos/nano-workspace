92f2abeeffc8e3367569ce7dbab2f84d4b783e82
Dimitrios Siganos <dimitris@siganos.org>
2021-10-17 16:35:43 +0200

    --beta, --test, --rpc switches

--- rpc/python/account_representative_set.py
+++ rpc/python/account_representative_set.py
@@ -4,6 +4,8 @@ import requests
 import json
 import argparse
 
+import common
+
 # example usage:
 #./account_representative_set.py \
 #   1CB0F1AD005BA3A9AAA111DFC9BC0FE120EF8BCC5C7920AD398935997BA8A5EC \
@@ -14,19 +16,31 @@ RPCURL = 'http://[::1]:7076'
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-b', '--beta', action='store_true', default=False,
+                       help='use beta network')
+    group.add_argument('-t', '--test', action='store_true', default=False,
+                       help='use test network')
+
+    parser.add_argument('--rpc',
+                        help='RPC URL to contact')
+
     parser.add_argument('wallet',
                         help='the wallet ID')
+
     parser.add_argument('account',
-                        help='the account ID')
+                        help='the account ID in nano_ format')
+
     parser.add_argument('representative',
-                        help='the representative ID')
+                        help='the representative ID in nano_ format')
+
     return parser.parse_args()
 
-def post(session, params, timeout=60):
-    resp = session.post(RPCURL, json=params, timeout=timeout)
-    return resp.json()
-
 args = parse_args()
+
+rpc_url = common.get_rpc_url(args)
+print('RPC URL = %s' % rpc_url)
 
 params = {
   'action'         : 'account_representative_set',
@@ -36,5 +50,5 @@ params = {
 }
 
 session = requests.Session()
-result = post(session, params)
+result = common.post(session, params, rpc_url)
 print(json.dumps(result, indent=4))
