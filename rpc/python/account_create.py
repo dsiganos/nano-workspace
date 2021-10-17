9d1df102da1f3843d1a0ac10dc0f93453bb11c98
Dimitrios Siganos <dimitris@siganos.org>
2021-10-17 16:22:58 +0200

    --beta, --test, --rpc switches

--- rpc/python/account_create.py
+++ rpc/python/account_create.py
@@ -5,22 +5,33 @@
 import requests
 import json
 import argparse
-import os
-import binascii
+
+import common
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-b', '--beta', action='store_true', default=False,
+                       help='use beta network')
+    group.add_argument('-t', '--test', action='store_true', default=False,
+                       help='use test network')
+
+    parser.add_argument('--rpc',
+                        help='RPC URL to contact')
+
     parser.add_argument('-i', '--index', type=int,
                         help='account index to create')
+
     parser.add_argument('wallet',
                         help='wallet ID')
+
     return parser.parse_args()
 
-def post(session, params, timeout=5):
-    resp = session.post('http://[::1]:7076', json=params, timeout=5)
-    return resp.json()
-
 args = parse_args()
+
+rpc_url = common.get_rpc_url(args)
+print('RPC URL = %s' % rpc_url)
 
 params = {
   'action': 'account_create',
@@ -31,5 +42,5 @@ if args.index != None:
     params['index'] = str(args.index)
 
 session = requests.Session()
-result = post(session, params)
+result = common.post(session, params, rpc_url)
 print(json.dumps(result, indent=4))
--- rpc/python/account_list.py
+++ rpc/python/account_list.py
@@ -5,20 +5,30 @@
 import requests
 import json
 import argparse
-import os
-import binascii
+
+import common
 
 def parse_args():
     parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-b', '--beta', action='store_true', default=False,
+                       help='use beta network')
+    group.add_argument('-t', '--test', action='store_true', default=False,
+                       help='use test network')
+
+    parser.add_argument('--rpc',
+                        help='RPC URL to contact')
+
     parser.add_argument('wallet',
                         help='wallet ID')
+
     return parser.parse_args()
 
-def post(session, params, timeout=5):
-    resp = session.post('http://[::1]:7076', json=params, timeout=5)
-    return resp.json()
-
 args = parse_args()
+
+rpc_url = common.get_rpc_url(args)
+print('RPC URL = %s' % rpc_url)
 
 params = {
   'action': 'account_list',
@@ -26,5 +36,5 @@ params = {
 }
 
 session = requests.Session()
-result = post(session, params)
+result = common.post(session, params, rpc_url)
 print(json.dumps(result, indent=4))
