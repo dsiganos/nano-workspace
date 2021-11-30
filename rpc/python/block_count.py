c9a0ddc86fd2f5c1915deaa7e1ba0756241a9064
Dimitrios Siganos <dimitris@siganos.org>
2021-11-30 14:51:40 +0200

    block_count.py: add ability to print continuous csv values

--- rpc/python/block_count.py
+++ rpc/python/block_count.py
@@ -3,8 +3,11 @@
 import requests
 import json
 import argparse
+import time
+import datetime
 
 import common
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -18,24 +21,53 @@ def parse_args():
     parser.add_argument('--rpc',
                         help='RPC URL to contact')
 
+    parser.add_argument('--csv', action='store_true', default=False,
+                        help='sample every 10 seconds and write a CSV data')
+
     return parser.parse_args()
+
+
+def get_block_count(session, rpc_url):
+    params = {'action' : 'block_count'}
+    result = common.post(session, params, rpc_url)
+    count = int(result['count'])
+    unchecked = int(result['unchecked'])
+    cemented = int(result['cemented'])
+    return result, count, unchecked, cemented
+
+
+def produce_csv_values(session, rpc_url):
+    print('count, timestamp (UTC), count, unchecked, cemented')
+    i = 0
+    lasttime = 0
+    while True:
+        if time.time() - lasttime > 10:
+            timestamp = datetime.datetime.utcnow().strftime("%H:%M:%S")
+            _, count, unchecked, cemented = get_block_count(session, rpc_url)
+            print('%s, %s, %s, %s, %s' % (i, timestamp, count, unchecked, cemented))
+            i += 1
+            lasttime = time.time()
+        time.sleep(0.1)
+
+
+def single_block_count(session, rpc_url):
+    result, count, unchecked, cemented = get_block_count(session, rpc_url)
+    print(json.dumps(result, indent=4))
+    print('count:      {:>15,}'.format(count))
+    print('unchecked:  {:>15,}'.format(unchecked))
+    print('cemented:   {:>15,}'.format(cemented))
+    print('uncemented: {:>15,}'.format(count - cemented))
+    print('total:      {:>15,}'.format(count + unchecked))
+
 
 args = parse_args()
 
 rpc_url = common.get_rpc_url(args)
 print('RPC URL = %s' % rpc_url)
 
-params = {'action' : 'block_count'}
-
 session = requests.Session()
-result = common.post(session, params, rpc_url)
-print(json.dumps(result, indent=4))
 
-count = int(result['count'])
-unchecked = int(result['unchecked'])
-cemented = int(result['cemented'])
-print('count:      {:>15,}'.format(count))
-print('unchecked:  {:>15,}'.format(unchecked))
-print('cemented:   {:>15,}'.format(cemented))
-print('uncemented: {:>15,}'.format(count - cemented))
-print('total:      {:>15,}'.format(count + unchecked))
+if args.csv:
+    produce_csv_values(session, rpc_url)
+else:
+    single_block_count(session, rpc_url)
