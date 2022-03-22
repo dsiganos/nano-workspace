5c5f2f9c371b8e83e2c188399227fc11e2a96f9f
Dimitrios Siganos <dimitris@siganos.org>
2022-03-22 02:52:09 +0100

    Wallet password and locking scripts

---
+++ rpc/python/wallet_lock.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# lock a wallet
+
+import requests
+import json
+import argparse
+
+import common
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-b', '--beta', action='store_true', default=False,
+                       help='use beta network')
+    group.add_argument('-t', '--test', action='store_true', default=False,
+                    help='use test network')
+
+    parser.add_argument('--rpc',
+                        help='RPC URL to contact')
+
+    parser.add_argument("wallet")
+
+    return parser.parse_args()
+
+args = parse_args()
+
+rpc_url = common.get_rpc_url(args)
+print('RPC URL = %s' % rpc_url)
+
+params = {
+  'action': 'wallet_lock',
+  'wallet': args.wallet,
+}
+
+session = requests.Session()
+result = common.post(session, params, rpc_url)
+print(json.dumps(result, indent=4))
