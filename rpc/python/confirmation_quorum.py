e649fd48654737803105ec8f6764e659b409a370
Dimitrios Siganos <dimitris@siganos.org>
2024-02-27 12:48:58 +0100

    confirmation quorum prettier print

--- rpc/python/confirmation_quorum.py
+++ rpc/python/confirmation_quorum.py
@@ -38,3 +38,17 @@ if args.peerdetails:
 session = requests.Session()
 result = common.post(session, params, rpc_url)
 print(json.dumps(result, indent=4))
+
+quorum_delta          = int(result['quorum_delta'])
+online_weight_minimum = int(result['online_weight_minimum'])
+online_stake_total    = int(result['online_stake_total'])
+trended_stake_total   = int(result['trended_stake_total'])
+peers_stake_total     = int(result['peers_stake_total'] )
+
+print('online_weight_quorum_percent:', result['online_weight_quorum_percent'])
+print('quorum_delta         :', "{:,} * (10**30)".format(quorum_delta // 10**30))
+print('online_weight_minimum:', "{:,} * (10**30)".format(online_weight_minimum // 10**30))
+print('online_stake_total   :', "{:,} * (10**30)".format(online_stake_total // 10**30))
+print('trended_stake_total  :', "{:,} * (10**30)".format(trended_stake_total // 10**30))
+print('peers_stake_total    :', "{:,} * (10**30)".format(peers_stake_total // 10**30))
+print()
